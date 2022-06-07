d9c855224baf4a82d51bc3955619cd5b3df77472
Wilson <wilsonevangelista22.we@gmail.com>
2022-06-06 21:49:57 -0300

    componente contador

--- Seção 7 - Introdução aos Componentes/componente-intro/src/App.vue
+++ Seção 7 - Introdução aos Componentes/componente-intro/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h1>Contadores </h1>
-    <button @click="contador--">Subtrair</button>
-    <span class="numero">{{ contador }}</span>
-    <button @click="contador++">Adicionar</button>
+    <app-contador></app-contador>
+    <app-contador></app-contador>
+    <app-contador></app-contador>
   </div>
 
 </template>
@@ -11,22 +11,10 @@
 <script>
 
 export default {
-  data() {
-    return {
-      contador: 0
-
-    }
-  }
+  
 }
 </script>
 
 <style>
-  .numero{
-    margin-left: 5px;
-    padding-left: 15px;
-    margin-right: 5px;
-    padding-right: 15px;
-    border: solid 1px;
-    border-radius: 2px;
-  }
+  
 </style>
