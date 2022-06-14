6522f144474cde03966af0fa8979ecc54c349985
Wilson <wilsonevangelista22.we@gmail.com>
2022-06-13 22:12:49 -0300

    comunicação entre componentes

--- Seção 8 - Comunicação Entre Componentes/comunicacao/src/components/UsuarioInfo.vue
+++ Seção 8 - Comunicação Entre Componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -2,12 +2,34 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ nome }}</strong></p>
+        <p>Nome do Invertido: <strong>{{ inverterNome() }}</strong></p>
+
+        <p>Nome do Usuário: <strong>{{ sobrenome }}</strong></p>
     </div>
 </template>
 
 <script>
 export default {
-    
+    // props: ['nome'],
+    props: {
+        nome: {
+            type: String,
+            // required: true,
+            default: 'Anônimo'
+        },
+        sobrenome: {
+            type: String,
+            default: function() {
+                return Array(10).fill(0).join(',')
+            }
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        }
+    }
 }
 </script>
 
